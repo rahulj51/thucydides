b9719def578c4b31bf01c7ab5b4ed105804bde98
Rahul Jain <rahulj51@gmail.com>
2012-07-14 07:05:35 +0800

    DAO for snapshots

---
+++ thucydides-core/src/main/java/net/thucydides/core/reports/html/history/JPATestResultSnapshotDAO.java
@@ -0,0 +1,15 @@
+package net.thucydides.core.reports.html.history;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rahul
+ * Date: 7/14/12
+ * Time: 7:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JPATestResultSnapshotDAO {
+
+
+
+
+}
